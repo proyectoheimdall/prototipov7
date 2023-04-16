c819829514f6944bf7a471048c47cc529e6f0277
daniel <daniel.fuentes@brgint.com>
2023-04-16 22:45:29 +0200

    Code .ino for ESP32 with library MQTTPubSubCliente_Generic

---
+++ heimdallv7_code/defines.h
@@ -0,0 +1,11 @@
+// **********************
+//		DEFINICIONES
+//***********************
+
+//#define MQTT_SERVER         "192.168.2.30"
+#define MQTT_SERVER           "9152c9a707da4be3b54b7e1e3621ff80.s2.eu.hivemq.cloud"
+#define MQTT_PORT             8883
+#define MQTT_USER             "heimdall"
+#define MQTT_PASS             "heimdall"
+#define WIFI_SSID             "HEIMDALL"
+#define WIFI_PASS             "heimdall"
